b8c9c5150feca410a5f2c45e052f75979e99bc5b
Dan Prudhomme <dannyprud@yahoo.com>
2022-05-24 21:55:43 -0500

    Correct file creation method through tutorial to practice with classes and objects

---
+++ C Sharpe Practice/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharpe_Practice
+{
+    internal class Book
+    {
+        public int pages;
+        public string title;
+        public string author;
+
+        public Book(int aPages, string aTitle, string aAuthor)
+        {
+            pages = aPages;
+            title = aTitle;
+            author = aAuthor;
+        }
+    }
+}
---
+++ C Sharpe Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharpe_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Book cat = new Book(4, "Cats R Us", "Madam Heart");
+            Console.WriteLine(cat.title + " by " + cat.author + " pages: " + cat.pages);
+            Console.ReadLine();
+        }
+    }
+}
